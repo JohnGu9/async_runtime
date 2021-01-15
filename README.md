b37c8d2fb5e1e70dc6286e298de8b28c8a742fd4
johngu_mac <johngumaster@outlook.com>
2021-01-15 14:42:27 +0800

    extract the framework code as a independent repo

--- README.md
+++
@@ -1,3 +0,0 @@
-# Async Runtime
-
-A simple framework similar with Flutter but without build GUI. Just for backend software. 
--- test/test.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "main_activity.h"
-
-int main(int argc, char **args)
-{
-    runApp(Object::create<MainActivity>());
-    return EXIT_SUCCESS;
-}
