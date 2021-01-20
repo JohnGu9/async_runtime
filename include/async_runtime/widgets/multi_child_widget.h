8dfbe25e5757f86872b753f3256de96c4892af2a
johngu_mac <johngumaster@outlook.com>
2021-01-20 15:35:13 +0800

    update multi child widget

--- include/async_runtime/widgets/multi_child_widget.h
+++ include/async_runtime/widgets/multi_child_widget.h
@@ -4,11 +4,13 @@
 
 class MultiChildWidget : public Widget
 {
+    friend MultiChildElement;
+
 public:
     MultiChildWidget(const Object::List<Object::Ref<Widget>> &children, Object::Ref<Key> key = nullptr);
+    MultiChildWidget(Object::Ref<Key> key = nullptr);
     Object::Ref<Element> createElement() override;
-
-    friend MultiChildElement;
+    virtual Object::Ref<MultiChildWidget> setChildren(Object::List<Object::Ref<Widget>> &&);
 
 protected:
     Object::List<Object::Ref<Widget>> _children;
--- src/widgets/multi_child_widget.cpp
+++ src/widgets/multi_child_widget.cpp
@@ -2,7 +2,15 @@
 
 MultiChildWidget::MultiChildWidget(const Object::List<Object::Ref<Widget>> &children, Object::Ref<Key> key) : _children(children), Widget(key) {}
 
+MultiChildWidget::MultiChildWidget(Object::Ref<Key> key) : _children({}), Widget(key) {}
+
 Object::Ref<Element> MultiChildWidget::createElement()
 {
     return Object::create<MultiChildElement>(Object::cast<>(this));
+}
+
+Object::Ref<MultiChildWidget> MultiChildWidget::setChildren(Object::List<Object::Ref<Widget>> &&children)
+{
+    this->_children = std::forward<Object::List<Object::Ref<Widget>>>(children);
+    return Object::cast<>(this);
 }
