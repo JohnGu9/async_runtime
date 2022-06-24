16543bf746d8fa687170642b268e57f21405a374
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-06-24 11:34:24 +0800

    remove lateref useless constructor

--- include/async_runtime/object/object/lateref.h
+++ include/async_runtime/object/object/lateref.h
@@ -14,12 +14,8 @@ public:
     explicit lateref() : super() {}
     lateref(std::nullptr_t) = delete;
 
-    // msvc something can't infer template type, so add two constructors below make code more suitable on windows
+    // linux can't infer correct template constructor that inherited from ref<T>
+    // two constructor below is to patch the problem
     lateref(const ref<T> &other) : super(other) {}
     lateref(ref<T> &&other) : super(std::move(other)) {}
-
-    template <typename R, typename std::enable_if<std::is_base_of<T, R>::value>::type * = nullptr>
-    lateref(const ref<R> &other) : super(other) {}
-    template <typename R, typename std::enable_if<std::is_base_of<T, R>::value>::type * = nullptr>
-    lateref(ref<R> &&other) : super(std::move(other)) {}
 };
