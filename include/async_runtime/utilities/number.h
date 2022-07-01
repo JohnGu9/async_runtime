c68f1ec018718e599617a1f87a84c21a33194fd5
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-07-01 13:52:14 +0800

    optimize Number implement

--- include/async_runtime/utilities/number.h
+++ include/async_runtime/utilities/number.h
@@ -2,6 +2,8 @@
 #include "../object/object.h"
 #include <cmath>
 
+// https://en.cppreference.com/w/cpp/language/types
+
 class Number : public virtual Object
 {
 public:
@@ -10,48 +12,113 @@ public:
     public:
         enum Value
         {
+            Short,
             Integer,
+            Long,
+            LongLong,
+            Float,
             Double,
         };
     };
 
-    virtual int toInt() = 0;
-    virtual double toDouble() = 0;
     virtual Number::Type::Value type() = 0;
+    template <typename T>
+    class Basic;
 
-    class Integer;
-    class Double;
+    class Short;    // at least 16 bits
+    class Integer;  // at least 16 bits
+    class Long;     // at least 32 bits
+    class LongLong; // at least 64 bits
+
+    class Float;  // 32 bits
+    class Double; // 64 bits
 };
 
-class Number::Integer : public Number
+template <typename T>
+class Number::Basic : public Number
 {
 public:
-    const int value;
-    Integer(int value) : value(value) {}
+    const T value;
+    Basic(T value) : value(value) {}
 
-    int toInt() override;
-    double toDouble() override;
+    size_t hashCode() override
+    {
+        return std::hash<T>()(value);
+    }
+    void toStringStream(std::ostream &os) override
+    {
+        os << value;
+    }
+    bool operator==(ref<Object> other) override;
+};
+
+class Number::Short : public Number::Basic<short>
+{
+public:
+    using Number::Basic<short>::Basic;
+    Number::Type::Value type() override; // Number::Type::Short
+};
+
+class Number::Integer : public Number::Basic<int>
+{
+public:
+    using Number::Basic<int>::Basic;
     Number::Type::Value type() override; // Number::Type::Integer
-
-    bool operator==(ref<Object> other) override;
-    size_t hashCode() override;
-    void toStringStream(std::ostream &os) override;
 };
 
-class Number::Double : public Number
+class Number::Long : public Number::Basic<long>
 {
 public:
-    const double value;
-    Double(double value) : value(value) {}
-
-    int toInt() override;
-    double toDouble() override;
-    Number::Type::Value type() override; // Number::Type::Double
-
-    bool operator==(ref<Object> other) override;
-    size_t hashCode() override;
-    void toStringStream(std::ostream &os) override;
+    using Number::Basic<long>::Basic;
+    Number::Type::Value type() override; // Number::Type::Long
 };
+
+class Number::LongLong : public Number::Basic<long long>
+{
+public:
+    using Number::Basic<long long>::Basic;
+    Number::Type::Value type() override; // Number::Type::LongLong
+};
+
+class Number::Float : public Number::Basic<float>
+{
+public:
+    using Number::Basic<float>::Basic;
+    Number::Type::Value type() override; // Number::Type::Float
+};
+
+class Number::Double : public Number::Basic<double>
+{
+public:
+    using Number::Basic<double>::Basic;
+    Number::Type::Value type() override; // Number::Type::Double
+};
+
+template <typename T>
+bool Number::Basic<T>::operator==(ref<Object> other)
+{
+    if (auto ptr = dynamic_cast<Number *>(other.get()))
+    {
+        switch (ptr->type())
+        {
+        case Number::Type::Short:
+            return this->value == dynamic_cast<Number::Short *>(ptr)->value;
+        case Number::Type::Integer:
+            return this->value == dynamic_cast<Number::Integer *>(ptr)->value;
+        case Number::Type::Long:
+            return this->value == dynamic_cast<Number::Long *>(ptr)->value;
+        case Number::Type::LongLong:
+            return this->value == dynamic_cast<Number::LongLong *>(ptr)->value;
+        case Number::Type::Float:
+            return this->value == dynamic_cast<Number::Float *>(ptr)->value;
+        case Number::Type::Double:
+            return this->value == dynamic_cast<Number::Double *>(ptr)->value;
+        default:
+            break;
+        }
+    }
+    return false;
+}
 
 template <>
 class ref<Number> : public _async_runtime::RefImplement<Number>
@@ -65,7 +132,12 @@ public:
     template <typename R, typename std::enable_if<std::is_base_of<Number, R>::value>::type * = nullptr>
     ref(ref<R> &&other) noexcept : super(std::move(other)) {}
 
+    ref(short value) : super(Object::create<Number::Short>(value)) {}
     ref(int value) : super(Object::create<Number::Integer>(value)) {}
+    ref(long value) : super(Object::create<Number::Long>(value)) {}
+    ref(long long value) : super(Object::create<Number::LongLong>(value)) {}
+
+    ref(float value) : super(Object::create<Number::Float>(value)) {}
     ref(double value) : super(Object::create<Number::Double>(value)) {}
 
 protected:
--- src/utilities/number.cpp
+++ src/utilities/number.cpp
@@ -1,45 +1,19 @@
 #include "async_runtime/utilities/number.h"
 
-int Number::Integer::toInt() { return value; }
+// Short
+Number::Type::Value Number::Short::type() { return Number::Type::Short; }
 
-double Number::Integer::toDouble() { return value; }
-
+// Integer
 Number::Type::Value Number::Integer::type() { return Number::Type::Integer; }
 
-bool Number::Integer::operator==(ref<Object> other)
-{
-    if (auto ptr = dynamic_cast<Integer *>(other.get()))
-    {
-        return this->value == ptr->value;
-    }
-    return false;
-}
+// Long
+Number::Type::Value Number::Long::type() { return Number::Type::Long; }
 
-size_t Number::Integer::hashCode()
-{
-    return std::hash<int>()(value);
-}
+// LongLong
+Number::Type::Value Number::LongLong::type() { return Number::Type::LongLong; }
 
-void Number::Integer::toStringStream(std::ostream &os) { os << value; }
+// Float
+Number::Type::Value Number::Float::type() { return Number::Type::Float; }
 
-int Number::Double::toInt() { return static_cast<int>(value); }
-
-double Number::Double::toDouble() { return value; }
-
+// Double
 Number::Type::Value Number::Double::type() { return Number::Type::Double; }
-
-bool Number::Double::operator==(ref<Object> other)
-{
-    if (auto ptr = dynamic_cast<Double *>(other.get()))
-    {
-        return this->value == ptr->value;
-    }
-    return false;
-}
-
-size_t Number::Double::hashCode()
-{
-    return std::hash<double>()(value);
-}
-
-void Number::Double::toStringStream(std::ostream &os) { os << value; }
