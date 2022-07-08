6e338b86cf35f4880dd433192827db80de925ae9
JohnGu9 <31389215+JohnGu9@users.noreply.github.com>
2022-07-08 13:41:06 +0800

    fix size_t compare issue

--- include/async_runtime/utilities/number/number_impl.h
+++ include/async_runtime/utilities/number/number_impl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "number.h"
+#include <type_traits>
 
 template <typename T>
 int Number::cmp(const size_t &s, const T &other)
@@ -8,7 +9,7 @@ int Number::cmp(const size_t &s, const T &other)
     if (other < 0)
         return 1;
 
-    auto unsignedNumber = static_cast<unsigned long long>(other);
+    auto unsignedNumber = static_cast<typename std::make_unsigned<T>::type>(other);
     if (s < unsignedNumber)
         return -1;
     else if (s > unsignedNumber)
@@ -23,10 +24,10 @@ int Number::cmp(const T &other, const size_t &s)
     if (other < 0)
         return -1;
 
-    auto unsignedNumber = static_cast<unsigned long long>(other);
-    if (s < unsignedNumber)
+    auto unsignedNumber = static_cast<typename std::make_unsigned<T>::type>(other);
+    if (unsignedNumber < s)
         return -1;
-    else if (s > unsignedNumber)
+    else if (unsignedNumber > s)
         return 1;
     else
         return 0;
